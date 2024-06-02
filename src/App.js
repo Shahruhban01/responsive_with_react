416d60cf60d8d3e50762fb21ae49bc8bb0e0e6f3
Shahruhban01 <shahruhban01@gmail.com>
2024-06-02 20:51:13 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,50 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [menuShow, setMenuShow] = useState(false);
+  const [ShowModal, setShowModal] = useState(false);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <button className='enquire_btn' onClick={() => setShowModal(true)}>Enquire Now</button>
+    <div className={`modalOverlay ${ (ShowModal) ? 'showModalOverlay' : '' }`} onClick={() => setShowModal(false)}></div>
+      <div className={`ModalDiv ${ (ShowModal) ? 'showModalDiv' : '' }`} >
+        {/* <h1></h1> */}
+        <div class="signup-form">
+        <h2>Enquire Now <button onClick={() => setShowModal(false)}>&times;</button></h2>
+        <form action="#" method="post">
+            <div class="form-group">
+                <label for="username">Username</label>
+                <input type="text" id="username" name="username" required />
+            </div>
+            <div class="form-group">
+                <label for="email">Email</label>
+                <input type="email" id="email" name="email" required />
+            </div>
+            <div class="form-group">
+                <label for="password">Password</label>
+                <input type="password" id="password" name="password" required />
+            </div>
+            <div class="form-group">
+                <button type="submit">Sign Up</button>
+            </div>
+        </form>
     </div>
+      </div>
+    {/* </div> */}
+
+    <button className='hamburger' onClick={() => setMenuShow(!menuShow)}>{ (menuShow) ? <span>&times;</span> : <span>&#9776;</span>}</button>
+    <div className={`main ${(menuShow) ? 'showMenu' : ''}`}>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Career</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+    </>
   );
 }
 
