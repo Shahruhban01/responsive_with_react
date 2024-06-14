12d6845e3b191e6fe88d0a4ce703861058d6b916
Shahruhban01 <shahruhban01@gmail.com>
2024-06-14 13:19:10 +0530

    react-notifications

--- src/App.js
+++ src/App.js
@@ -3,13 +3,28 @@ import './App.css';
 import { useState } from 'react';
 import { questions } from './Data/faqQuestion';
 import Faqs from './components/Faqs';
+import 'react-notifications/lib/notifications.css';
+import { NotificationContainer, NotificationManager } from 'react-notifications';
 
 function App() {
   // const [showAns, setshowAns] = useState(questions[0].id);
   const [menuShow, setMenuShow] = useState(false);
   const [ShowModal, setShowModal] = useState(false);
+  let n = ["array", "of", "four", "Items"];
+  
+  let submitSuccess = () => {
+    NotificationManager.info('Info message');
+    NotificationManager.success("Success!", 'Testing');
+    NotificationManager.error('Error message', 'Click me!', 5000, () => {
+      alert('callback');
+      });
+    NotificationManager.warning('Warning message', 'Close after 3000ms', 3000);
+  }
   return (
-    <>
+    <div className='App'>
+      <NotificationContainer />
+
+
       <button className='hamburger' onClick={() => setMenuShow(!menuShow)}>{(menuShow) ? <span>&times;</span> : <span>&#9776;</span>}</button>
       <div className={`main ${(menuShow) ? 'showMenu' : ''}`}>
         <ul>
@@ -20,6 +35,7 @@ function App() {
           <li>Contact</li>
         </ul>
       </div>
+      <button onClick={submitSuccess}>Show Notifications Messages</button>
 
       {/* <div>
         <h1 align="center">Frequently Asked Questions (FAQs)</h1>
@@ -39,6 +55,20 @@ function App() {
 
         </div>
       </div> */}
+
+
+      {/* Faqs Start */}
+      <Faqs />
+      {/* Faqs End */}
+
+      <ul>
+        {n.map((items, i) => {
+          return (
+            <li key={i}>{items}</li>
+          )
+        })}
+      </ul>
+
       {/* Enquire Now Modal Starts */}
       <button className='enquire_btn' onClick={() => setShowModal(true)}>Enquire Now</button>
       <div className={`modalOverlay ${(ShowModal) ? 'showModalOverlay' : ''}`} onClick={() => setShowModal(false)}></div>
@@ -59,7 +89,7 @@ function App() {
               <input type="password" id="password" name="password" required />
             </div>
             <div className="form-group">
-              <button type="submit">Sign Up</button>
+              <button type="submit" onClick={submitSuccess}>Sign Up</button>
             </div>
           </form>
         </div>
@@ -67,15 +97,9 @@ function App() {
       {/* </div> */}
       {/* Enquire Now Modal Ends */}
 
-      {/* Faqs Start */}
-      <Faqs />
-      {/* Faqs End */}
 
 
-
-
-
-    </>
+    </div>
   );
 }
 
